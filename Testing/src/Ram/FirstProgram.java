62241d2e6070aeb84f5ccbe47914e2a2d2ed95c8
Arpitks1995 <Arpit9555413985@gmail.com>
2024-06-27 17:16:58 +0530

    First Commit by the US

--- Testing/src/Ram/FirstProgram.java
+++ Testing/src/Ram/FirstProgram.java
@@ -4,6 +4,7 @@ public class FirstProgram {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World I am Java");
+		System.out.println("Encapsulation By the Indaian Team");
 
 	}
 
