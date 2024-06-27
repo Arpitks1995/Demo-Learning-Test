b1ac0e35ab54345eae1f2b78d85cdc9a2655b2eb
Arpitks1995 <Arpit9555413985@gmail.com>
2024-06-27 22:29:31 +0530

    First commit by the Indian person

---
+++ TestMyLearningGitDemo/src/TestingJavaProgram/FirstProgram.java
@@ -0,0 +1,12 @@
+package TestingJavaProgram;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Hello java");
+		System.out.println("Code is Written by the Indian person");
+
+	}
+
+}
