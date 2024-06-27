7a3b19fc530e20c39b40d8b5d3f77816114c0d58
Arpitks1995 <Arpit9555413985@gmail.com>
2024-06-27 17:57:12 +0530

    My second commit by Indian

--- Testing/src/Ram/FirstProgram.java
+++ Testing/src/Ram/FirstProgram.java
@@ -4,7 +4,8 @@ public class FirstProgram {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World I am Java");
-		System.out.println("Encapsulation By the Indaian Team");
+		System.out.println("Encapsulation By the Indian Team");
+		System.out.println("Abstraction By the Indian Team ");
 
 	}
 
